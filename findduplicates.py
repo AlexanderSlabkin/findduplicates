e0bae01ba6189331011d7006ac385b35ff1e6309
Alexander <alexslabkin@gmail.com>
2019-05-15 19:00:57 +0300

    Output duplicates with paths to them but without right order

--- findduplicates.py
+++ findduplicates.py
@@ -14,14 +14,15 @@ FullNames = []
 UniqueNames = []
 for i in sys.argv[1:]:
     Files = myls(i)
-    print(Files[0])
     FileNames.append(Files[0])
-    UniqueNames.append(FileNames[0])
+    UniqueNames.append(set(FileNames[0]))
     print(len(FileNames[0]), len(UniqueNames[0]))
-    Duplicates = [x for x in UniqueNames[0] if FileNames[0].count(x) > 1]
+    UniqueDuplicates = [x for x in UniqueNames[0] if FileNames[0].count(x) > 1]
+    DuplicatesIndexes = [y for (y,x) in enumerate(FileNames[0]) if FileNames[0].count(x) > 1]
     FullNames.append(Files[1])
-print(len(myls(sys.argv[1])[0]), len(set(myls(sys.argv[1])[0])))
-print(f'Duplicates: {Duplicates}')
+    DuplicatesFull = [FullNames[0][i] for i in DuplicatesIndexes]
+print(f'Duplicates: {UniqueDuplicates}, lengths is {len(UniqueDuplicates)}')
+print(f'Duplicates with paths: {DuplicatesFull}, lengths is {len(DuplicatesFull)}')
 intersection = FileNames[0]
 #print(filenames)
 for i in FileNames[1:]:
