052fa798ace55853d96b826122e12e20d79fa1d4
Alexander <alexslabkin@gmail.com>
2019-05-15 18:40:31 +0300

    Nearly work with one input directory

--- findduplicates.py
+++ findduplicates.py
@@ -3,23 +3,29 @@ import subprocess
 
 def myls(path):
     buf = subprocess.check_output(['find', path, '-type', 'f']).decode('utf-8')
-    filenames = buf.split('\n')[:-1]
-    fullnames = []
-
-    for ind, i in enumerate(filenames):
-        fullnames.append(i)
-        filenames[ind] =i[i.rfind('/')+1:]
+    fullnames = buf.split('\n')[:-1]
+    filenames = []
+    for index, i in enumerate(fullnames):
+        filenames.append(i[i.rfind('/')+1:])
     return filenames, fullnames
 
-filenames = []
-#print(myls(sys.argv[1]))
+FileNames = []
+FullNames = []
+UniqueNames = []
 for i in sys.argv[1:]:
-    print(i)
-    filenames.append(set(myls(i)[0]))
+    Files = myls(i)
+    print(Files[0])
+    FileNames.append(Files[0])
+    UniqueNames.append(FileNames[0])
+    print(len(FileNames[0]), len(UniqueNames[0]))
+    Duplicates = [x for x in UniqueNames[0] if FileNames[0].count(x) > 1]
+    FullNames.append(Files[1])
 print(len(myls(sys.argv[1])[0]), len(set(myls(sys.argv[1])[0])))
-intersection = filenames[0]
-for i in filenames[1:]:
-    intersection = intersetion & i
+print(f'Duplicates: {Duplicates}')
+intersection = FileNames[0]
+#print(filenames)
+for i in FileNames[1:]:
+    intersection = intersection & i
 
-print(intersection)
+#print(intersection)
 print('Done.')
